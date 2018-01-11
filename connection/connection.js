9a4866142fcb5af6f6cdede6566999808794e939
Wenbiao Li <lixuyao.tony@gmail.com>
2018-01-10 21:20:55 -0500

    jawsDB connection

--- connection/connection.js
+++ connection/connection.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
 
+const connection = "mysql://j2vx52cwv4yx86mh:zwlhe8osumudwq2d@l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/b0mqhlbqyn8lcb0z";
 
-
-if (process.env.JAWSDB_URL) {
-	const connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	let connection = mysql.createConnection({
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+// if (process.env.JAWSDB_URL) {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+// 	connection = mysql.createConnection({
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
   
-  user: "root",
-  password: "root",
-  database: "resCallDB"
-  });
-};
+//   user: "root",
+//   password: "root",
+//   database: "resCallDB"
+//   });
+// };
 
 module.exports = connection;
 

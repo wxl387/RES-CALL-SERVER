228f96cfd7e135830de28119e17d382b27c57b01
Wenbiao Li <lixuyao.tony@gmail.com>
2018-01-10 20:56:50 -0500

    add test.js

--- connection/connection.js
+++ connection/connection.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-	socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-	user: "root",
-	password: "root",
-	database: "rescallDB"
-});
+
+const connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+  
+  user: "root",
+  password: "root",
+  database: "resCallDB"
+  });
+};
 
 module.exports = connection;
 
